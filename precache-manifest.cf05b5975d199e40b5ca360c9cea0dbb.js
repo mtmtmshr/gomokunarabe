6c436e7fa8a04d0ce26ffab5e1c449dddadbf632
mtmtmshr <s17t274@stu.kagawa-u.ac.jp>
2020-08-13 19:56:15 +0900

    Updates

---
+++ precache-manifest.cf05b5975d199e40b5ca360c9cea0dbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43201f000da8dd0cc3bd6f45475f98d3",
+    "url": "/gomokunarabe/index.html"
+  },
+  {
+    "revision": "fad1b63a488ff9cb7554",
+    "url": "/gomokunarabe/static/css/main.b792a898.chunk.css"
+  },
+  {
+    "revision": "642f11ce68d236595a80",
+    "url": "/gomokunarabe/static/js/2.a2c56392.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gomokunarabe/static/js/2.a2c56392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fad1b63a488ff9cb7554",
+    "url": "/gomokunarabe/static/js/main.319412d7.chunk.js"
+  },
+  {
+    "revision": "436657b68190da67409c",
+    "url": "/gomokunarabe/static/js/runtime-main.ce137558.js"
+  }
+]);
